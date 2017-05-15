2f05bc37dff43b6d19407c0d4c091a1e834d3306
ProSleeper <ingngnin@gmail.com>
2017-05-16 00:13:39 +0900

    First

---
+++ Assets/_Scripts/Crash.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crash : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("플레이어 충돌!");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Vector3 mouseMovePos;
+    Vector3 mouseClickPos;
+    Vector3 distance;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            mouseClickPos = Input.mousePosition;
+            mouseClickPos = Camera.main.ScreenToWorldPoint(mouseClickPos);
+            distance = this.gameObject.transform.position - mouseClickPos;
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            mouseMovePos = Input.mousePosition;
+            mouseMovePos = Camera.main.ScreenToWorldPoint(mouseMovePos);
+            this.transform.position =  mouseMovePos + distance;
+        }
+
+    }
+}
